493773fe9a14d1ec7ee96ce37dd4e564b69e074b
Zxilly <zhouxinyu1001@gmail.com>
2020-03-11 12:20:15 +0800

    get_work() add

--- func.py
+++ func.py
@@ -1,5 +1,6 @@
 import json
 import time
+from urllib.parse import unquote
 
 from PIL import Image
 # import cv2
@@ -47,9 +48,46 @@ def login(conv):
     return conv
 
 
+def get_course_list(conv):
+    course_list_url_list = []
+    course_list_host_url = 'http://mooc1-1.chaoxing.com'
+    course_list_url = 'http://mooc1-1.chaoxing.com/visit/courses'
+
+    course_list_html = conv.get(course_list_url, headers=headers)
+    html_obj = BeautifulSoup(course_list_html.content, "html.parser")
+    course_list_all_tag = html_obj.find_all("h3", class_='clearfix')
+    for course_one_tag in course_list_all_tag:
+        course_one_tag_info = course_one_tag.find("a")
+        # print(course_one_tag_info)
+        course_one_tag_url = course_list_host_url + course_one_tag_info.attrs['href'].replace(r"&amp;", r"&")
+        # print(course_one_tag_url)
+        course_list_url_list.append(course_one_tag_url)
+    return course_list_url_list
 
 
+def get_course_work(conv, course_url_list):
+    work_url_list = []
+    for course_url in course_url_list:
+        host_url = 'http://mooc1-1.chaoxing.com'
+        html_content = conv.get(course_url, headers=headers)
+        html_obj = BeautifulSoup(str(html_content.content.decode('utf-8').strip()), 'html.parser')
+        # print(html_obj)
+        # exit(0)
+        work_url_quote = html_obj.find("a", string=r'作业  ')['data']
+        work_url = unquote(work_url_quote)
+        work_url = host_url + work_url
+        work_url_list.append(work_url)
+        # print(type(work_url))
+        # print(work_url)
+        # work_html = conv.get(work_url,headers=headers)
+    return work_url_list
 
+
+def get_work_info(conv, url_list):
+    work_info_list = []
+    for url in url_list:
+        html = conv.get(url,headers=headers)
+        html_obj = BeautifulSoup(str(html.content),"html.parser")
 
 
 """
--- main.py
+++ main.py
@@ -1,20 +1,24 @@
-import requests,json
+import requests
 
 from func import *
 
 conv = requests.session()
 
-conv_login = login(conv) # 登录
+conv_login = login(conv)  # 登录
 
 print("登录成功")
 
-course_info_dict = backclazzdata(conv_login) # 抓取课程列表
+course_url_list = get_course_list(conv_login)  # 获取课程对应url
+# 返回一个含有所有课程url的列表
 
-gettask(course_info_dict,conv_login)
+course_work_list = get_course_work(conv_login, course_url_list)
+# 返回一个含有所有作业url的列表
+
+work_info_list = get_work_info(conv_login, course_url_list)
+# 返回一个含有所有待做作业信息的列表
 
 
 
-"""
-if __name__ == '__main__':
-    pass
-"""
+# course_info_dict = backclazzdata(conv_login) # 抓取课程列表
+
+# gettask(course_info_dict,conv_login)
